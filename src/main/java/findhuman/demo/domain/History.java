1133136a15f7cba97dce0831ea8cd91d2053d360
HRLEE <apalsl123@gmail.com>
2019-10-31 17:32:02 +0900

    Update History.java

--- src/main/java/findhuman/demo/domain/History.java
+++ src/main/java/findhuman/demo/domain/History.java
@@ -16,7 +16,7 @@ import java.util.Objects;
 @NoArgsConstructor
 @AllArgsConstructor
 public class History {
-
+//
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
